4d92a978b09b5ea666595f018cf6fae3bb875742
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-04-30 19:44:48 +0200

    👀 Implement Catching

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ module.exports = (suffix, type) => {
     let types = ['info', 'warn', 'error', `process`]
 
     if (!types.includes(type)) {
-        throw new Error("Invalid logging type.")
+        throw new Error("Invalid log message type.")
+    }
+    
+    if (!suffix) {
+        throw new Error("Invalid log message.")
+    }
+    
+    if (!type) {
+        throw new Erorr("Invalid log message type.")
     }
 
     console.log(`[${type}] ${suffix}`)
