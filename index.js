f14d879f8101ed893d38e13e8222603bd0505b20
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-05-01 10:38:02 +0200

    ✨ Introducing v2, a major overhaul that changes everything.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = (suffix, type) => {
+exports.custom = (suffix, type) => {
     if (type.length > 20) {
         throw new Error("Invalid message type. TypeLengthError: Type too long.")
     }
@@ -12,4 +12,36 @@ module.exports = (suffix, type) => {
     }
 
     console.log(`[${type}] ${suffix}`)
+}
+
+exports.info = (suffix) => {
+    if(!suffix) {
+        throw new Error("Invalid log message. MessageNotFound: There was no message to be logged.")
+    }
+    
+    console.log(`[info] ${suffix}`)
+}
+
+exports.err = (suffix) => {
+    if(!suffix) {
+        throw new Error("Invalid log message. MessageNotFound: There was no message to be logged.")
+    }
+    
+    console.log(`[err] ${suffix}`)
+}
+
+exports.warn = (suffix) => {
+    if(!suffix) {
+        throw new Error("Invalid log message. MessageNotFound: There was no message to be logged.")
+    }
+    
+    console.log(`[warn] ${suffix}`)
+}
+
+exports.process = (suffix) => {
+    if(!suffix) {
+        throw new Error("Invalid log message. MessageNotFound: There was no message to be logged.")
+    }
+    
+    console.log(`[process] ${suffix}`)
 }
